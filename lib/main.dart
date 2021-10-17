85d6741b53650a5dd821671b0757c507cafa5086
Slava <sh-e-95@mail.ru>
2021-10-18 03:06:35 +0500

    .

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData(primaryColor: Colors.blue),
+        theme: ThemeData(primaryColor: Colors.purple),
         home: Scaffold(
             appBar: AppBar(
               title: const Text('Mandelbrot Set'),
