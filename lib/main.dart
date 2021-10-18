9d323e8339f70b42180bb6247330f12bb74c9e73
Slava <sh-e-95@mail.ru>
2021-10-19 00:35:02 +0500

    add normal button

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,74 @@
-import 'dart:html';
+// ignore_for_file: unnecessary_null_comparison
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'Logic/Models/complex.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MaterialApp(
+      theme: ThemeData(primaryColor: Colors.purple),
+      home: const MandelbrotSet(),
+    ));
 
-// ignore: use_key_in_widget_constructors
-class MyApp extends StatelessWidget {
+class MandelbrotSet extends StatefulWidget {
+  const MandelbrotSet({Key? key}) : super(key: key);
+
+  @override
+  _MandelbrotSet createState() => _MandelbrotSet();
+}
+
+class _MandelbrotSet extends State<MandelbrotSet> {
+  double zoom = 6;
+  bool isVisibleSet = false;
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        theme: ThemeData(primaryColor: Colors.purple),
-        home: Scaffold(
-            appBar: AppBar(
-              title: const Text('Mandelbrot Set'),
-            ),
-            floatingActionButton: FloatingActionButton(
-              onPressed: () {
-                //myPaint.paint(canvas, size);
-              },
-              child: const Text('Start'),
-            ),
-            body: Center(child: LayoutBuilder(
-              builder: (BuildContext context, BoxConstraints constrains) {
-                return CustomPaint(
-                    painter: myPaint,
-                    size: Size(constrains.maxWidth, constrains.maxHeight));
-              },
-            ))));
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Mandelbrot Set'),
+          backgroundColor: Colors.purple,
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              isVisibleSet = true;
+            });
+          },
+          child: const Text('Start'),
+          backgroundColor: Colors.purple,
+        ),
+        body: isVisibleSet
+            ? GestureDetector(
+                onTap: () {
+                  setState(() {
+                    zoom /= 2;
+                  });
+                },
+                child: LayoutBuilder(
+                  builder: (BuildContext context, BoxConstraints constrains) {
+                    return CustomPaint(
+                        painter: MyDrawer(scale: zoom),
+                        size: Size(constrains.maxWidth, constrains.maxHeight));
+                  },
+                ),
+              )
+            : null,
+        drawerScrimColor: Colors.blue);
   }
 }
 
-CustomPainter? myPaint = MyDrawer();
-
 class MyDrawer extends CustomPainter {
+  final double scale;
+  MyDrawer({required this.scale});
   @override
   void paint(Canvas canvas, Size size) {
+    if (size.width == null || size.height == null) {
+      return;
+    }
     for (double x = 0; x < size.width; x++) {
       for (double y = 0; y < size.height; y++) {
         int count = 0;
-        double _x = (x - size.width / 2.0) / (size.width / 6.0);
-        double _y = (y - size.height / 2.0) / (size.height / 6.0);
+        double _x = (x - size.width / 2.0) / (size.width / scale);
+        double _y = (y - size.height / 2.0) / (size.height / scale);
         Complex z = Complex(0, 0);
         Complex c = Complex(_x, _y);
 
@@ -70,7 +96,5 @@ class MyDrawer extends CustomPainter {
   }
 
   @override
-  bool shouldRepaint(CustomPainter oldDelegate) {
-    return true;
-  }
+  bool shouldRepaint(CustomPainter oldDelegate) => false;
 }
